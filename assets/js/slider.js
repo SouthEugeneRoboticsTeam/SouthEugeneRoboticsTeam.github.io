88d5ef46e7cbd1197288dd632e820f9c0930c1f9
Evan Hopper-Moore <evanhoppermoore@gmail.com>
2017-01-29 16:51:21 -0800

    Change slider to be smooth

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,33 +1,44 @@
 (function($) {
 
-	$(document).ready(function () {
-	    var img_array = [1, 2, 3, 4, 5, 6],
-	        newIndex = 0,
-	        index = 0,
-	        interval = 7500;
+    $(document).ready(function() {
+        var img_array = [1, 2, 3, 4, 5, 6],
+            newIndex = 0,
+            index = 0,
+            interval = 4500;
 
-	    (function changeBg() {
-	        index = (index + 1) % img_array.length;
+        (function changeBg() {
+            index = (index + 1) % img_array.length;
 
-	        $('.slider').css('backgroundImage', function () {
-				$('.slider').css('opacity', 0.1);
+            $('.slider').css('opacity', .9);
+            var switchDir = false;
+            var itsTime = false;
 
-				var appear = setInterval(function() {
-					var opacity = $('.slider').css('opacity');
+            var appear = setInterval(function() {
+                var opacity = $('.slider').css('opacity');
 
-					if (opacity < 1) {
-						$('.slider').css('opacity', parseFloat(opacity) + 0.02);
-					} else {
-						clearInterval(appear);
-					}
-				}, 20)
+                if (opacity <= 0.05) {
+                    switchDir = true;
+					itsTime = true;
+                }
+                if (opacity >= 1) {
 
-	            return 'url(/assets/images/slider/' + img_array[index] + '.jpg)';
-	        });
+                    clearInterval(appear);
+                }
 
-	        setTimeout(changeBg, interval);
+                if (switchDir) {
+                    $('.slider').css('opacity', parseFloat(opacity) + 0.02);
+                } else {
+                    $('.slider').css('opacity', parseFloat(opacity) - 0.02);
+                }
 
-	    })();
-	});
+				if (itsTime) {
+					$('.slider').css('backgroundImage', 'url(/assets/images/slider/' + img_array[index] + '.jpg)');
+				}
+            }, 20);
+
+            setTimeout(changeBg, interval);
+
+        })();
+    });
 
 })(jQuery);
