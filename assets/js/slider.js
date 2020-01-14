58c3a9bc1062b8d3d02677bad11b2e9f77d1e153
William <whou125@gmail.com>
2020-01-13 21:45:58 -0800

    Fix slider 2020

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,7 +1,7 @@
 (function ($) {
 
     $(document).ready(function () {
-        var img_array = [1, 2, 3, 4, 6, 7, 9, 10, 11, 12, 13, 14];
+        var img_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
         var index = 0;
         var interval = 8000;
 
